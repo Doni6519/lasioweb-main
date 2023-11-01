792273495e84198b2ab229f702202a850bde98a7
meshack <meshackdaniel225@gmail.com>
2023-11-01 03:15:14 +0100

    1112023

--- app/admin/[[...index]]/page.jsx
+++ app/admin/[[...index]]/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import config from '@/sanity.config'
+import config from "@/sanity.config";
 import { NextStudio } from "next-sanity/studio";
 
 export default function AdminPage(){
---
+++ sanity.config.js
@@ -0,0 +1,18 @@
+import schemas from "./sanity/schemas";
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+
+const config = defineConfig({
+    projectId: "q6lrshxy",
+    dataset: "production",
+    title: "lasioweb",
+    apiVersion: "2023-9-18",
+    basePath: "/admin",
+    plugins: [
+        deskTool(), visionTool()
+    ],
+    schema: {types: schemas}
+})
+
+export default config;
