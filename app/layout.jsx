eff9bf6245daad6398b055415a7c09800bf35660
meshack <meshackdaniel225@gmail.com>
2023-11-11 15:53:55 +0100

    11112023

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,38 @@
-import '../styles/globals.css'
-import Nav from 'components/Nav'
-import Footer from '@/components/Footer'
+import "../styles/globals.css";
+import Nav from "components/Nav";
+import Footer from "@/components/Footer";
 
 export const metadata = {
-    title: "LasioWeb",
-    description: "Get visually apealling websites tailored to your business"
-}
+  title: "LasioWeb",
+  description: "Get visually apealling websites tailored to your business",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <head>
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css"  rel="stylesheet" />
-      <link rel="icon" type="image/x-icon" href="/assets/images/logo.png" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+        />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css"
+          rel="stylesheet"
+        />
+        <link rel="icon" type="image/x-icon" href="/assets/images/logo.png" />
       </head>
-    <body className='font-sans'>
-        <main className='app'>
-            <Nav />
-            {children}
-            <Footer />
+      <body className="font-sans">
+        <main className="app">
+          <Nav />
+          {children}
+          <Footer />
         </main>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
         <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
-    </body>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
