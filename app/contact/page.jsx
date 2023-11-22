bb720a796bf4c96bc561519aef21e9dd64736b86
meshack <meshackdaniel225@gmail.com>
2023-11-22 14:43:16 +0100

    22112023

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -30,6 +30,8 @@ const contact = () => {
     try {
       await sendContactForm(values);
       setState(initState);
+      document.getElementById("toast").innerHTML =
+        '<div id="toast-simple" class="flex items-center mb-5 w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse text-gray-500 bg-green-100 divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert"><svg class="w-5 h-5 text-blue-600 dark:text-blue-500 rotate-45" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"/></svg><div class="ps-4 text-sm font-normal">Message sent successfully.</div></div>';
     } catch (error) {
       setState((prev) => ({
         ...prev,
@@ -49,12 +51,54 @@ const contact = () => {
         <p className="text-white">Lets talk about your ideas.</p>
         <section>
           <form
-            // action="/"
-            // method="post"
+            action="/contact"
+            method="post"
             className="bg-white md:w-3/6 lg:w-2/6 p-10 drop-shadow md:absolute"
           >
+            <div id="toast"></div>
             {error && (
-              <p>{error} hi</p>
+              <div
+                id="toast-warning"
+                className="mb-5 flex items-center w-full max-w-xs p-4 text-gray-500 bg-orange-100 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 justify-between"
+                role="alert"
+              >
+                <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+                  <svg
+                    class="w-5 h-5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="red"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+                  </svg>
+                  <span class="sr-only">Warning icon</span>
+                </div>
+                <div class="ms-3 text-sm font-normal">{error}.</div>
+                <button
+                  type="button"
+                  class="ms-auto -mx-1.5 -my-1.5 bg-orange-100 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                  data-dismiss-target="#toast-warning"
+                  aria-label="Close"
+                >
+                  <span class="sr-only">Close</span>
+                  <svg
+                    class="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="red"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                </button>
+              </div>
             )}
             <h1 className="text-base md:text-2xl font-bold uppercase mb-5">
               Send us a message
