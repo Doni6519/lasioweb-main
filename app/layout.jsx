eddb34eb9bb962a13a861b9ffeaa48401abbdb1e
meshack <meshackdaniel225@gmail.com>
2024-04-12 08:25:20 +0100

    12 april 2024

--- app/layout.jsx
+++ app/layout.jsx
@@ -29,11 +29,11 @@ const RootLayout = ({ children }) => {
       </head>
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-5031ECEFSM"></script>
 <script>
+  {
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
-  gtag('config', 'G-5031ECEFSM');
+  gtag('config', 'G-5031ECEFSM');}
 </script>
       <body className="font-sans" id="body">
       <ChakraProvider>
