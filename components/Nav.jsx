978bf067b92f78e2880610067f0e3ca0553d9de5
meshack <meshackdaniel225@gmail.com>
2024-05-07 12:30:42 +0100

    may6

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -56,7 +56,7 @@ const Nav = () => {
           </li>
           <li onClick={handleClick}>
             <a
-              href="/services"
+              href="https://lasioweb.com/services"
               className="block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
             >
               Services
@@ -143,12 +143,12 @@ const Nav = () => {
               </Link>
             </li>
             <li>
-              <Link
-                href="/services"
+              <a
+                href="https://lasioweb.com/services"
                 className="block font-bold py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Services
-              </Link>
+              </a>
             </li>
             <li>
               <a
