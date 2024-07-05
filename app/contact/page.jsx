f8adcd5d0d79aa044889fc2dd3e61938dc4800c5
meshack <meshackdaniel225@gmail.com>
2024-07-05 11:45:46 +0100

    july5

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -196,7 +196,7 @@ const contact = () => {
               onChange={handleChange}
               required
             ></textarea>
-            <div className="mt-5 mx-auto w-32">
+            <div className="mt-5 mx-auto w-32 ms-3">
               <Button
                 disabled={true}
                 variant="solid"
@@ -208,7 +208,7 @@ const contact = () => {
               <button
                 type="submit"
                 id="submitButton"
-                style={{ width: 109 , marginLeft: -90}}
+                style={{ width: 109 , marginLeft: -86}}
                 class="absolute text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
                 Send
